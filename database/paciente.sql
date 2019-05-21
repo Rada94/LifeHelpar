eac77adc41faa54a627eb93af082049de1a99b9f
Rada94 <rodrigo.rada94@outlook.com>
2019-05-21 16:43:27 -0500

    bases de datos 1 actualizacion

--- database/paciente.sql
+++ database/paciente.sql
@@ -1,14 +1,21 @@
 create database if not exists LifeHelper;
 use LifeHelper;
 create table if not exists pacientes(
-id_paciente int auto_increment not null unique,
+id_paciente int not null auto_increment unique primary key,
 nombre_paciente text not null,
 apellidop_paciente text not null,
 apellidom_paciente text not null,
+fnacimiento_paciente date not null,
 edad_paciente int not null,
-domicilio_paciente text not null,
+genero_paciente varchar(12) not null,
+calleynumero_paciente text not null, /*Calle y numero de casa del paciente*/
+colonia_paciente text not null,
 telefonoc_paciente varchar(20) not null,
 mobil_paciente varchar(20) not null,
-genero_paciente varchar(12) not null,
-foto_paciente blob not null
+email_paciente text not null unique,
+status_paciente text not null, /*Status del paciente*/
+expediente_paciente longtext not null, /*Comentarios de doctor al paciente*/
+fcitas_paciente datetime not null, /*Fecha y hora del cometario del doctor al paciente*/
+crefnombre_paciente text not null, /*Nombre del referenciado del paciente*/
+telefonoref_paciente text not null /*Numero de telefono del referenciado del paciente*/
 );
