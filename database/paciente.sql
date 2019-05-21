07727b9fb622d89535247cf13332dff318cb6ac0
Rada94 <rodrigo.rada94@outlook.com>
2019-05-21 16:52:50 -0500

    actualizacion a la parte de paciente

--- database/paciente.sql
+++ database/paciente.sql
@@ -10,12 +10,16 @@ edad_paciente int not null,
 genero_paciente varchar(12) not null,
 calleynumero_paciente text not null, /*Calle y numero de casa del paciente*/
 colonia_paciente text not null,
-telefonoc_paciente varchar(20) not null,
-mobil_paciente varchar(20) not null,
+celular_paciente varchar(20) not null,
+telefono_opcinal_paciente varchar(20),
 email_paciente text not null unique,
 status_paciente text not null, /*Status del paciente*/
 expediente_paciente longtext not null, /*Comentarios de doctor al paciente*/
 fcitas_paciente datetime not null, /*Fecha y hora del cometario del doctor al paciente*/
 crefnombre_paciente text not null, /*Nombre del referenciado del paciente*/
+crefparentezco_paciente text not null, /*Referencia Familiar*/
+telrefparentezco_paciente text not null, /*Telefono del familiar*/
+crefparentezco_opcional_paciente text, /*Referencia opcional*/
+telrefparentezco_opcional_paciente text, /*Telefono de la referencia opcional*/
 telefonoref_paciente text not null /*Numero de telefono del referenciado del paciente*/
 );
