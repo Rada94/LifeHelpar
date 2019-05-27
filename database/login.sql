a6b9622bd51d5101396428dcc8fb65f46a01a3b9
Rada94 <rodrigo.rada94@outlook.com>
2019-05-27 08:38:06 -0500

    Se borra todo lo del proyecto, para dar paso al nuevo

--- database/login.sql
+++
@@ -1,7 +0,0 @@
-create database if not exists lifehelper;
-use lifehelper;
-create table if not exists login(
-perfil varchar(20) not null,
-emails varchar(255) not null,
-passwords varchar(8) not null
-)engine=InnoDB;
